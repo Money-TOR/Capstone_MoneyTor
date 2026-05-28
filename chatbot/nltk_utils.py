6df733a876580c217971c4841ce1c159c7d57110
Justin Valencius Cahyadi <justinvalen04@gmail.com>
2026-05-28 21:05:26 +0700

    Update library sastrawi

--- chatbot/nltk_utils.py
+++ chatbot/nltk_utils.py
@@ -1,12 +1,14 @@
 import numpy as np
 import nltk
-# nltk.download('punkt')
-from nltk.stem.porter import PorterStemmer
-stemmer = PorterStemmer()
+
+from Sastrawi.Stemmer.StemmerFactory import StemmerFactory
+
+factory = StemmerFactory()
+stemmer = factory.create_stemmer()
 
 
 def tokenize(sentence):
-    return nltk.word_tokenize(sentence)
+    return nltk.word_tokenize(sentence.lower())
 
 
 def stem(word):
@@ -15,9 +17,11 @@ def stem(word):
 
 def bag_of_words(tokenized_sentence, words):
     sentence_words = [stem(word) for word in tokenized_sentence]
+
     bag = np.zeros(len(words), dtype=np.float32)
+
     for idx, w in enumerate(words):
-        if w in sentence_words: 
+        if w in sentence_words:
             bag[idx] = 1
 
     return bag
